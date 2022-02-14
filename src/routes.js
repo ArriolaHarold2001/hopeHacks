3a48b3ee6ae5d46770dbeb22cde4694ce6ce52f9
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-02-14 18:48:41 -0500

    got a response from API

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
+const request = require("request");
 
 const apiKey = "9d662556d4944aaaa71c567219994789";
 const url = `https://newsapi.org/v2/everything?q=Apple&from=2022-02-14&sortBy=popularity&apiKey=${apiKey}`;
 
 router.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
+  const reqs = request(url, (err, res, body) => {
+    if (err) {
+      return;
+    } else {
+      return body;
+    }
   });
+  res.status(200).send(reqs);
 });
 
 module.exports = router;
